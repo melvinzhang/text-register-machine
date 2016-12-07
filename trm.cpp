7bad4d5f7741848b70b40f65211cf510582b3534
melvinzhang <melvinzhang@gmail.com>
2016-12-07 10:10:45 +0800

    move globals into struct TRM

--- trm.cpp
+++ trm.cpp
@@ -46,123 +46,128 @@ struct Instr {
     int n;
 };
 
-vector< Instr > program;
-map< int, queue<Code> > mem;
+struct TRM {
+    vector< Instr > program;
+    map< int, queue<Code> > mem;
 
-void push(int i, Code c) {
-    mem[i].push(c);
-}
-
-int cases(int i) {
-    queue<Code>& Rn = mem[i];
-    if (Rn.empty()) {
-        return 1;
-    } else if (Rn.front() == O) {
-        Rn.pop();
-        return 2;
-    } else if (Rn.front() == H) {
-        Rn.pop();
-        return 3;
+    void push(int i, Code c) {
+        mem[i].push(c);
     }
-}
 
-void print_registers() {
-    for (map< int, queue<Code> >::iterator it=mem.begin(); it != mem.end(); it++) {
-        cout << "R" << it->first << ":";
-        queue<Code> content = it->second;
-        while (content.empty() == false) {
-            Code c = content.front();
-            content.pop();
-            if (c == O) {
-                cout << "1";
-            } else {
-                cout << "#";
-            }
-        }
-        cout << endl;
-    }
-}
-
-string op2string(Op o) {
-    switch (o) {
-        case Add1: return "Add1";
-        case Add0: return "Add#";
-        case Inc:  return "Inc";
-        case Dec:  return "Dec";
-        case Case: return "Case";
-    }
-}
-
-string instr2string(Instr in) {
-     return op2string(in.op) + " " + to_string(in.n);
-}
-    
-void print_program() {
-    for ( auto &p : program) {
-        cout << instr2string(p) << endl;
-    }
-}
-
-void eval(int pc) {
-    while (pc < program.size()) {
-        Instr in = program[pc];
-        //cout << "pc:" + to_string(pc) + " " + "instr: " + instr2string(in) << endl;
-        switch (in.op) {
-            case Add1:
-                push(in.n, O);
-                pc++;
-                break;
-
-            case Add0:
-                push(in.n, H);
-                pc++;
-                break;
-
-            case Inc:
-                pc += in.n;
-                break;
-
-            case Dec:
-                pc -= in.n;
-                break;
-
-            case Case:
-                pc += cases(in.n);
-                break;
+    int cases(int i) {
+        queue<Code>& Rn = mem[i];
+        if (Rn.empty()) {
+            return 1;
+        } else if (Rn.front() == O) {
+            Rn.pop();
+            return 2;
+        } else if (Rn.front() == H) {
+            Rn.pop();
+            return 3;
         }
     }
-}
-    
-void load_program() {
-    cin.sync_with_stdio(false);
-    
-    char curr;
 
-    int n = 0;
-    int c = 0;
-    
-    while (cin >> curr) {
-        if (curr == '1') {
-            if (c > 0) {
-                n = 0;
-                c = 0;
+    void print_registers() {
+        for (map< int, queue<Code> >::iterator it=mem.begin(); it != mem.end(); it++) {
+            cout << "R" << it->first << ":";
+            queue<Code> content = it->second;
+            while (content.empty() == false) {
+                Code c = content.front();
+                content.pop();
+                if (c == O) {
+                    cout << "1";
+                } else {
+                    cout << "#";
+                }
             }
-            n++;
-        } else if (curr == '#') {
-            c++;
-            if (c == 1) {
-                program.push_back({Add1, n});
-            } else {
-                program.back().op = (Op)c;
+            cout << endl;
+        }
+    }
+
+    string op2string(Op o) {
+        switch (o) {
+            case Add1: return "Add1";
+            case Add0: return "Add#";
+            case Inc:  return "Inc";
+            case Dec:  return "Dec";
+            case Case: return "Case";
+        }
+    }
+
+    string instr2string(Instr in) {
+         return op2string(in.op) + " " + to_string(in.n);
+    }
+        
+    void print_program() {
+        for ( auto &p : program) {
+            cout << instr2string(p) << endl;
+        }
+    }
+
+    void eval(int pc) {
+        while (pc < program.size()) {
+            Instr in = program[pc];
+            //cout << "pc:" + to_string(pc) + " " + "instr: " + instr2string(in) << endl;
+            switch (in.op) {
+                case Add1:
+                    push(in.n, O);
+                    pc++;
+                    break;
+
+                case Add0:
+                    push(in.n, H);
+                    pc++;
+                    break;
+
+                case Inc:
+                    pc += in.n;
+                    break;
+
+                case Dec:
+                    pc -= in.n;
+                    break;
+
+                case Case:
+                    pc += cases(in.n);
+                    break;
             }
         }
     }
-}
+        
+    void load_program() {
+        cin.sync_with_stdio(false);
+        
+        char curr;
+
+        int n = 0;
+        int c = 0;
+        
+        while (cin >> curr) {
+            if (curr == '1') {
+                if (c > 0) {
+                    n = 0;
+                    c = 0;
+                }
+                n++;
+            } else if (curr == '#') {
+                c++;
+                if (c == 1) {
+                    program.push_back({Add1, n});
+                } else {
+                    program.back().op = (Op)c;
+                }
+            }
+        }
+    }
+};
+
+
 
 int main() {
-    load_program();
-    //print_program();
-    eval(0);
-    print_registers();
+    TRM m;
+    m.load_program();
+    m.print_program();
+    m.eval(0);
+    m.print_registers();
     return 0;
 }

930f48258967464c86b51f7493632e987ac292b3
melvinzhang <melvinzhang@gmail.com>
2016-12-07 13:04:28 +0800

    show animation on cerr, at the end output content of R1 to cout

--- trm.cpp
+++ trm.cpp
@@ -73,35 +73,49 @@ struct TRM {
         int lines = 0;
         for (map< int, queue<Code> >::iterator it=mem.begin(); it != mem.end(); it++) {
             int col = 4;
-            cout << "R" << it->first << ": ";
+            cerr << "R" << it->first << ": ";
             queue<Code> content = it->second;
             while (content.empty() == false) {
                 Code c = content.front();
                 content.pop();
                 if (c == O) {
-                    cout << "1";
+                    cerr << "1";
                 } else {
-                    cout << "#";
+                    cerr << "#";
                 }
                 col++;
                 if (col == 80) {
-                    cout << endl;
-                    cout << "    ";
+                    cerr << endl;
+                    cerr << "    ";
                     col = 4;
                     lines++;
                 }
             }
-            cout << endl;
+            cerr << endl;
             lines++;
         }
         return lines;
     }
 
+    void output() {
+        queue<Code> content = mem[1];
+        while (content.empty() == false) {
+            Code c = content.front();
+            content.pop();
+            if (c == O) {
+                cout << "1";
+            } else {
+                cout << "#";
+            }
+        }
+        cout << endl;
+    }
+
     void clear_previous(int lines, int fps) {
         this_thread::sleep_for(chrono::microseconds(1000000 / fps));
         for (int i = 0; i < lines; i++) {
-            cout << "\x1B[1A"; // Move the cursor up one line
-            cout << "\x1B[2K"; // Erase the entire current line
+            cerr << "\x1B[1A"; // Move the cursor up one line
+            cerr << "\x1B[2K"; // Erase the entire current line
         }
     }
 
@@ -121,7 +135,7 @@ struct TRM {
 
     void print_program() {
         for ( auto &p : program) {
-            cout << instr2string(p) << endl;
+            cerr << instr2string(p) << endl;
         }
     }
 
@@ -158,11 +172,11 @@ struct TRM {
             if (cnt > 1 && fps > 0) {
                 clear_previous(lines + 1, fps);
             }
-            cout << "IN: " + instr2string(in) << endl;
+            cerr << "IN: " + instr2string(in) << endl;
             lines = print_registers();
         }
-        cout << endl;
-        cout << "Done. Executed " << cnt << " instructions.\n";
+        cerr << endl;
+        cerr << "Done. Executed " << cnt << " instructions.\n";
     }
 
     void load_program() {
@@ -196,7 +210,7 @@ int main(int argc, char *argv[]) {
     TRM m;
     m.load_program();
     m.print_program();
-    cout << endl;
+    cerr << endl;
     if (argc == 1) {
         m.eval(1000, 0);
     } else if (argc == 2) {
@@ -204,5 +218,6 @@ int main(int argc, char *argv[]) {
     } else if (argc == 3) {
         m.eval(atoi(argv[1]), atoi(argv[2]));
     }
+    m.output();
     return 0;
 }
